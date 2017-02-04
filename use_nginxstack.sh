0e1a7996cef10a72247de8b0c0c4613683db1393
jazzman <jazzman.svi@yandex.ru>
2017-02-04 20:21:06 +0200

    add nodejs installer

--- use_nginxstack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PATH="/home/jazzman/nginxstack-1.10.2-0/nginx/sbin:/home/jazzman/nginxstack-1.10.2-0/php/bin:/home/jazzman/nginxstack-1.10.2-0/mysql/bin:/home/jazzman/nginxstack-1.10.2-0/common/bin:$PATH"
-BITNAMI_ROOT="/home/jazzman/nginxstack-1.10.2-0"
-export BITNAMI_ROOT
-
-exec /bin/bash --noprofile --norc
-                    
