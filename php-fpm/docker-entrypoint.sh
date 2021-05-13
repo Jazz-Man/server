87ca8ae54df896bfd952224bcacebe5afbf6c27e
Vasyl Sokolyk <vasyl@onsweb.nl>
2021-05-13 23:00:16 +0300

    rename entrypoint for php-fpm

--- php-fpm/docker-entrypoint.sh
+++ php-fpm/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-export CPU=$(grep -c ^processor /proc/cpuinfo);
-export TOTALMEM=$(free -m | awk '/^Mem:/{print $2}');
+CPU=$(grep -c ^processor /proc/cpuinfo);
+TOTALMEM=$(free -m | awk '/^Mem:/{print $2}');
 
 if [[ "$CPU" -le "2" ]]; then TOTALCPU=2; else TOTALCPU="${CPU}"; fi
 
@@ -12,19 +12,10 @@ if [[ -z $PHP_MAX_SPARE_SERVERS ]]; then PHP_MAX_SPARE_SERVERS="${TOTALCPU}"; fi
 if [[ -z $PHP_MEMORY_LIMIT ]]; then PHP_MEMORY_LIMIT=$(($TOTALMEM / 2)); fi
 if [[ -z $PHP_MAX_CHILDREN ]]; then PHP_MAX_CHILDREN=$(($TOTALCPU * 2)); fi
 
-echo "CPU: ${CPU}";
-echo "TOTALCPU: ${TOTALCPU}";
-echo "TOTALMEM: ${TOTALMEM}";
-echo "PHP_START_SERVERS: ${PHP_START_SERVERS}";
-echo "PHP_MIN_SPARE_SERVERS: ${PHP_MIN_SPARE_SERVERS}"
-echo "PHP_MAX_SPARE_SERVERS: ${PHP_MAX_SPARE_SERVERS}"
-echo "PHP_MEMORY_LIMIT: ${PHP_MEMORY_LIMIT}"
-echo "PHP_MAX_CHILDREN: ${PHP_MAX_CHILDREN}"
-
-esh -o $PHP_CONF_PREFIX/php-fpm.conf /template/php-fpm.conf
-esh -o $PHP_CONF_PREFIX/php-fpm.d/frontend.conf /template/php-fpm-frontend.conf
-esh -o $PHP_CONF_PREFIX/php-fpm.d/backend.conf /template/php-fpm-backend.conf
-esh -o $PHP_CONF_PREFIX/conf.d/zz-php.ini /template/zz-php.ini
+esh -o $PHP_CONF_PREFIX/php-fpm.conf /template-php-fpm/php-fpm.conf
+esh -o $PHP_CONF_PREFIX/php-fpm.d/frontend.conf /template-php-fpm/php-fpm-frontend.conf
+esh -o $PHP_CONF_PREFIX/php-fpm.d/backend.conf /template-php-fpm/php-fpm-backend.conf
+esh -o $PHP_CONF_PREFIX/conf.d/zz-php.ini /template-php-fpm/zz-php.ini
 
 
 composer global config sort-packages true
